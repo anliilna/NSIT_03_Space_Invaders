70abf7e489057d48ca2013df4e31f8520a1960a8
anliilna <113678529+anliilna@users.noreply.github.com>
2022-09-20 10:09:13 +0200

    Add files via upload

---
+++ NSIT_03_POO_si/documents/space.py
@@ -0,0 +1,9 @@
+import pygame  # necessaire pour charger les images et les sons
+
+
+class Joueur() : # classe pour créer le vaisseau du joueur
+    def __init__(self) :
+        pass
+
+class player():
+    def __init__(self, )
