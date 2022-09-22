db2fe437f35a71a0ffbd830d34c96836ec4ab359
anliilna <113678529+anliilna@users.noreply.github.com>
2022-09-22 10:22:08 +0200

    Add files via upload

--- NSIT_03_POO_si/documents/space.py
+++ NSIT_03_POO_si/documents/space.py
@@ -1,9 +1,73 @@
 import pygame  # necessaire pour charger les images et les sons
+import random
+import math
 
+class Joueur():  # classe pour créer le vaisseau du joueur
+    def __init__(self):
+        self.position = 400
+        self.image = pygame.image.load("vaisseau.png")
+        self.sens = "O"
+        self.vitesse = 5
+        self.score = 0
 
-class Joueur() : # classe pour créer le vaisseau du joueur
-    def __init__(self) :
-        pass
+    def deplacer(self):  # fait bouger le vaisseau de joueur
+        if (self.sens == "droite") and (self.position < 740):
+            self.position = self.position + self.vitesse
+        elif (self.sens == "gauche") and (self.position > 0):
+            self.position = self.position - self.vitesse
 
-class player():
-    def __init__(self, )
+    def tirer(self):  # envoie une munition
+        self.sens = "O"
+
+    def marquer(self):  # augmente le score
+        self.score = self.score + 1
+
+class Balle():  # classe pour créer les munitions
+    def __init__(self, tireur):
+        self.tireur = tireur
+        self.depart = tireur.position + 16
+        self.hauteur = 492
+        self.image = pygame.image.load("balle.png")
+        self.etat = "chargee"
+        self.vitesse = 5
+
+    def bouger(self):  # dirige la balle vers les ennemis
+        if self.etat == "chargee":
+            self.depart = self.tireur.position + 16
+            self.hauteur = 492
+        elif self.etat == "tiree":
+            self.hauteur = self.hauteur - self.vitesse
+
+        if self.hauteur < 0:
+            self.etat = "chargee"
+
+    # v recharge une balle quand la précedente touche un ennemi
+    def toucher(self, vaisseau):
+        if (math.fabs(self.hauteur - vaisseau.hauteur) < 40) and (math.fabs(self.depart - vaisseau.depart) < 40):
+            self.etat = "chargee"
+            return True
+
+class Ennemi():  # classe pour créer les ennemis
+    NbEnnemis = 6
+
+    def __init__(self):
+        self.depart = random.randint(1, 700)
+        self.hauteur = 10
+        self.type = random.randint(1, 2)
+        self.vitesse = 5
+        self.image = pygame.image.load('invader1.png')
+
+    def avancer(self):  # fait avancer un ennemi vers le bas de l'écran
+        self.hauteur = self.hauteur + self.vitesse
+
+    # v fait disparaitre un ennemi quand il est touché par une munition
+    def disparaitre(self):
+        self.depart = random.randint(1, 700)
+        self.hauteur = 10
+        self.type = random.randint(1, 2)
+        if (self.type == 1):
+            self.image = pygame.image.load("invader1.png")
+            self.vitesse = 1
+        elif (self.type == 2):
+            self.image = pygame.image.load("invader2.png")
+            self.vitesse = 2
--- NSIT_03_POO_si/documents/space_invaders_5.py
+++ NSIT_03_POO_si/documents/space_invaders_5.py
@@ -1,51 +1,51 @@
-import pygame # importation de la librairie pygame
+import pygame  # importation de la librairie pygame
 import space
-import sys # pour fermer correctement l'application
+import sys  # pour fermer correctement l'application
 
-# lancement des modules inclus dans pygame
-pygame.init() 
+pygame.init()  # lancement des modules inclus dans pygame
 
-# création d'une fenêtre de 800 par 600
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Space Invaders") 
-# chargement de l'image de fond
-fond = pygame.image.load('background.png')
+screen = pygame.display.set_mode((800, 600)) # création d'une fenêtre de 800 par 600
+pygame.display.set_caption("Space Invaders")
+fond = pygame.image.load('background.png')  # chargement de l'image de fond
 
-# creation du joueur
-player = space.Joueur()
-# creation de la balle
-tir = space.Balle(player)
+player = space.Joueur()  # creation du joueur
+tir = space.Balle(player)  # creation de la balle
 tir.etat = "chargee"
-# creation des ennemis
-listeEnnemis = []
+listeEnnemis = []  # creation des ennemis
+
 for indice in range(space.Ennemi.NbEnnemis):
     vaisseau = space.Ennemi()
     listeEnnemis.append(vaisseau)
-    
-### BOUCLE DE JEU  ###
-running = True # variable pour laisser la fenêtre ouverte
 
-while running : # boucle infinie pour laisser la fenêtre ouverte
-    # dessin du fond
-    screen.blit(fond,(0,0))
+# BOUCLE DE JEU
 
-    ### Gestion des événements  ###
-    for event in pygame.event.get(): # parcours de tous les event pygame dans cette fenêtre
-        if event.type == pygame.QUIT : # si l'événement est le clic sur la fermeture de la fenêtre
-            running = False # running est sur False
-            sys.exit() # pour fermer correctement
-       
-       # gestion du clavier
-        if event.type == pygame.KEYDOWN : # si une touche a été tapée KEYUP quand on relache la touche
-            if event.key == pygame.K_LEFT : # si la touche est la fleche gauche
-                player.sens = "gauche" # on déplace le vaisseau de 1 pixel sur la gauche
-            if event.key == pygame.K_RIGHT : # si la touche est la fleche droite
-                player.sens = "droite" # on déplace le vaisseau de 1 pixel sur la gauche
-            if event.key == pygame.K_SPACE : # espace pour tirer
+running = True  # variable pour laisser la fenêtre ouverte
+
+while running:  # boucle infinie pour laisser la fenêtre ouverte
+    screen.blit(fond, (0, 0))  # dessin du fond
+    # Gestion des événements
+    # v parcours de tous les event pygame dans cette fenêtre
+    for event in pygame.event.get():
+        # v si l'événement est le clic sur la fermeture de la fenêtre
+        if event.type == pygame.QUIT:
+            running = False  # running est sur False
+            sys.exit()  # pour fermer correctement
+
+        # gestion du clavier
+        # v si une touche a été tapée KEYUP quand on relache la touche
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:  # si la touche est la fleche gauche
+                # v on déplace le vaisseau de 1 pixel sur la gauche
+                player.sens = "gauche"
+            # v si la touche est la fleche droite
+            if event.key == pygame.K_RIGHT:
+                # v on déplace le vaisseau de 1 pixel sur la gauche
+                player.sens = "droite"
+            if event.key == pygame.K_SPACE:  # espace pour tirer
                 player.tirer()
                 tir.etat = "tiree"
 
-    ### Actualisation de la scene ###
+    # Actualisation de la scene
     # Gestions des collisions
     for ennemi in listeEnnemis:
         if tir.toucher(ennemi):
@@ -55,13 +55,16 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
     # placement des objets
     # le joueur
     player.deplacer()
-    screen.blit(tir.image,[tir.depart,tir.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur        
+    # v appel de la fonction qui dessine le vaisseau du joueur
+    screen.blit(tir.image, [tir.depart, tir.hauteur])
     # la balle
     tir.bouger()
-    screen.blit(player.image,[player.position,500]) # appel de la fonction qui dessine le vaisseau du joueur
+    # v appel de la fonction qui dessine le vaisseau du joueur
+    screen.blit(player.image, [player.position, 500])
     # les ennemis
     for ennemi in listeEnnemis:
         ennemi.avancer()
-        screen.blit(ennemi.image,[ennemi.depart, ennemi.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur
-        
-    pygame.display.update() # pour ajouter tout changement à l'écran
+        # v appel de la fonction qui dessine le vaisseau du joueur
+        screen.blit(ennemi.image, [ennemi.depart, ennemi.hauteur])
+
+    pygame.display.update()  # pour ajouter tout changement à l'écran
