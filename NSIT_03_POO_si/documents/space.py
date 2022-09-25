766558273184f37cefe0e7ecd1f4ceda05e002c5
anliilna <113678529+anliilna@users.noreply.github.com>
2022-09-25 16:37:08 +0200

    Add files via upload

--- NSIT_03_POO_si/documents/space.py
+++ NSIT_03_POO_si/documents/space.py
@@ -4,16 +4,17 @@ import math
 
 class Joueur():  # classe pour créer le vaisseau du joueur
     def __init__(self):
-        self.position = 400
-        self.image = pygame.image.load("vaisseau.png")
+        self.position = 500 - 32
+        self.image = pygame.image.load("yellow_heart.png")
         self.sens = "O"
-        self.vitesse = 5
+        self.vitesse = 10
         self.score = 0
+        self.pv = 20
 
     def deplacer(self):  # fait bouger le vaisseau de joueur
-        if (self.sens == "droite") and (self.position < 740):
+        if (self.sens == "droite") and (self.position < 870):
             self.position = self.position + self.vitesse
-        elif (self.sens == "gauche") and (self.position > 0):
+        elif (self.sens == "gauche") and (self.position > 95):
             self.position = self.position - self.vitesse
 
     def tirer(self):  # envoie une munition
@@ -25,16 +26,16 @@ class Joueur():  # classe pour créer le vaisseau du joueur
 class Balle():  # classe pour créer les munitions
     def __init__(self, tireur):
         self.tireur = tireur
-        self.depart = tireur.position + 16
-        self.hauteur = 492
+        self.depart = tireur.position
+        self.hauteur = 437
         self.image = pygame.image.load("balle.png")
         self.etat = "chargee"
-        self.vitesse = 5
+        self.vitesse = 8
 
     def bouger(self):  # dirige la balle vers les ennemis
         if self.etat == "chargee":
-            self.depart = self.tireur.position + 16
-            self.hauteur = 492
+            self.depart = self.tireur.position
+            self.hauteur = 437
         elif self.etat == "tiree":
             self.hauteur = self.hauteur - self.vitesse
 
@@ -43,31 +44,38 @@ class Balle():  # classe pour créer les munitions
 
     # v recharge une balle quand la précedente touche un ennemi
     def toucher(self, vaisseau):
-        if (math.fabs(self.hauteur - vaisseau.hauteur) < 40) and (math.fabs(self.depart - vaisseau.depart) < 40):
-            self.etat = "chargee"
-            return True
+        if (math.fabs(self.hauteur - vaisseau.hauteur) < 40):
+            if(math.fabs(self.depart - vaisseau.depart) < 40):
+                self.etat = "chargee"
+                return True
 
 class Ennemi():  # classe pour créer les ennemis
     NbEnnemis = 6
 
     def __init__(self):
-        self.depart = random.randint(1, 700)
+        self.depart = random.randint(90, 800)
         self.hauteur = 10
-        self.type = random.randint(1, 2)
+        self.type = random.randint(1, 4)
         self.vitesse = 5
-        self.image = pygame.image.load('invader1.png')
+        self.image = pygame.image.load('invader2.png')
 
     def avancer(self):  # fait avancer un ennemi vers le bas de l'écran
         self.hauteur = self.hauteur + self.vitesse
 
     # v fait disparaitre un ennemi quand il est touché par une munition
     def disparaitre(self):
-        self.depart = random.randint(1, 700)
+        self.depart = random.randint(90, 800)
         self.hauteur = 10
-        self.type = random.randint(1, 2)
+        self.type = random.randint(1, 4)
         if (self.type == 1):
             self.image = pygame.image.load("invader1.png")
             self.vitesse = 1
         elif (self.type == 2):
             self.image = pygame.image.load("invader2.png")
             self.vitesse = 2
+        elif (self.type == 3):
+            self.image = pygame.image.load("invader3.png")
+            self.vitesse = 3
+        elif (self.type == 4):
+            self.image = pygame.image.load("invader4.png")
+            self.vitesse = 4
--- NSIT_03_POO_si/documents/space_invaders_5.py
+++ NSIT_03_POO_si/documents/space_invaders_5.py
@@ -1,17 +1,54 @@
-import pygame  # importation de la librairie pygame
+# importation de la librairie pygame
+import pygame
 import space
-import sys  # pour fermer correctement l'application
+# pour fermer correctement l'application
+import sys
+# lancement des modules inclus dans pygame
+pygame.init()
 
-pygame.init()  # lancement des modules inclus dans pygame
-
-screen = pygame.display.set_mode((800, 600)) # création d'une fenêtre de 800 par 600
-pygame.display.set_caption("Space Invaders")
-fond = pygame.image.load('background.png')  # chargement de l'image de fond
-
-player = space.Joueur()  # creation du joueur
-tir = space.Balle(player)  # creation de la balle
+# musique de fond
+pygame.mixer.music.load("Metal_Crusher.mp3")
+# jouer la musique en boucle
+pygame.mixer.music.play(-1)
+# création d'une fenêtre de 1024 par 723
+screen = pygame.display.set_mode((1024, 723))
+pygame.display.set_caption("Mettaton fight")
+# chargement de l'image de fond
+fond = pygame.image.load('undertale_bg.jpg')
+# creation du joueur
+player = space.Joueur()
+# creation de la balle
+tir = space.Balle(player)
 tir.etat = "chargee"
-listeEnnemis = []  # creation des ennemis
+# creation des ennemis
+listeEnnemis = []
+pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+# création d'une liste pour l'animation
+frames = [pygame.image.load("Mettaton00.png"),
+          pygame.image.load("Mettaton00.png"),
+          pygame.image.load("Mettaton01.png"),
+          pygame.image.load("Mettaton02.png"),
+          pygame.image.load("Mettaton03.png"),
+          pygame.image.load("Mettaton04.png"),
+          pygame.image.load("Mettaton05.png"),
+          pygame.image.load("Mettaton06.png"),
+          pygame.image.load("Mettaton07.png"),
+          pygame.image.load("Mettaton08.png"),
+          pygame.image.load("Mettaton09.png"),
+          pygame.image.load("Mettaton10.png"),
+          pygame.image.load("Mettaton11.png"),
+          pygame.image.load("Mettaton12.png"),
+          pygame.image.load("Mettaton13.png"),
+          pygame.image.load("Mettaton14.png"),
+          pygame.image.load("Mettaton15.png")]
+
+# création d'un objet "temps" pour suivre le temps
+temps = pygame.time.Clock()
+
+# création d'une nouvelle variable qui va aider
+# à répéter le changement d'image de la liste
+value = 0
 
 for indice in range(space.Ennemi.NbEnnemis):
     vaisseau = space.Ennemi()
@@ -23,6 +60,23 @@ running = True  # variable pour laisser la fenêtre ouverte
 
 while running:  # boucle infinie pour laisser la fenêtre ouverte
     screen.blit(fond, (0, 0))  # dessin du fond
+
+    # animation 25fps
+    temps.tick(25)
+
+    # remet value à 0 si elle est plus grande que la longueur de la liste
+    if value >= len(frames):
+        value = 0
+
+    # ranger le sprite dans une variable image
+    image = frames[value]
+
+    # placement de l'image dans la fenêtre
+    screen.blit(image, (415, 62))
+
+    # augmentation de value après chaque changement d'image
+    value += 1
+
     # Gestion des événements
     # v parcours de tous les event pygame dans cette fenêtre
     for event in pygame.event.get():
@@ -42,16 +96,23 @@ while running:  # boucle infinie pour laisser la fenêtre ouverte
                 # v on déplace le vaisseau de 1 pixel sur la gauche
                 player.sens = "droite"
             if event.key == pygame.K_SPACE:  # espace pour tirer
+                if tir.etat == "chargee":
+                    shoot = pygame.mixer.Sound("shoot.wav")
+                    shoot.play()
                 player.tirer()
                 tir.etat = "tiree"
+            if event.key == pygame.K_ESCAPE:
+                sys.exit()
 
     # Actualisation de la scene
     # Gestions des collisions
     for ennemi in listeEnnemis:
         if tir.toucher(ennemi):
-            ennemi.disparaitre()
-            player.marquer()
+                ennemi.disparaitre()
+                player.marquer()
+
     print(f"Score = {player.score} points")
+    print(f"pv = {player.pv} pv")
     # placement des objets
     # le joueur
     player.deplacer()
@@ -60,7 +121,7 @@ while running:  # boucle infinie pour laisser la fenêtre ouverte
     # la balle
     tir.bouger()
     # v appel de la fonction qui dessine le vaisseau du joueur
-    screen.blit(player.image, [player.position, 500])
+    screen.blit(player.image, [player.position, 440])
     # les ennemis
     for ennemi in listeEnnemis:
         ennemi.avancer()
